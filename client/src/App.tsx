4c3a2cbc1ca5c10462af2c087f96f402e6fe21ab
arjunsingh14 <arjunsingh140601@gmail.com>
2023-01-11 20:57:29 +0800

    Setup page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import Landing from "./pages/Landing"
+import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
 
   return (
-    <main className="bg-primary w-screen h-screen">
+    <Router>
       <Navbar></Navbar>
-      <Landing></Landing>
-    </main>
-  )
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
