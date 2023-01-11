d24a17487c61115236d0287a5c627f2fbd9f0467
arjunsingh14 <arjunsingh140601@gmail.com>
2023-01-11 20:39:02 +0800

    Updated styling

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,27 +1,33 @@
-import newspaper from "../../public/newspaper.svg"
+import newspaper from "../assets/newspaper.svg";
 const Landing = () => {
   return (
-    <div className="mt-10">
-      <div className="flex w-full flex-wrap-reverse items-center justify-center">
+    <div className="mt-20">
+      <div className="flex w-full flex-wrap-reverse items-center justify-center gap-x-10">
         <section>
-          <h1 className="sm:text-2xl lg:text-3xl">
-            Choose your <span className="font-bold">trusted</span> sources
+          <h1 className= "text-4xl">
+            Choose your <br></br>
+            <span className="font-bold">trusted</span> sources
           </h1>
           <div>
-            <p className="sm:text-md lg:text-lg mt-1">
-              In the age of misinformation it's hard to 
+            <p className="sm:text-md mt-4">
+              In the age of misinformation it's hard to
               <br></br>
-              establish a baseline for reputable sources.
+              block out irreputable articles on the internet.
+              <br></br>
+              Bugle aims to solve that issue by allowing <br></br>you to control
+              which sources you like.
             </p>
           </div>
-          <button className="rounded-full bg-secondary px-5 mt-3 hover:bg-white">Sign up</button>
+          <button className="rounded-full text-white bg-secondary px-7 py-4 mt-3 hover:bg-black transition">
+            Sign up
+          </button>
         </section>
-        <figure className="shrink-0 sm:px-5">
-          <img src={newspaper} alt="" />
-        </figure>
+        <div className="shrink-1 sm:px-5">
+          <img src={newspaper} className="object-containe" alt="" />
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Landing
+export default Landing;
