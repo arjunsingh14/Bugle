0fe7d51e78aaf020b397d5efa04da0090425d2d1
arjunsingh14 <arjunsingh140601@gmail.com>
2023-01-14 21:11:56 +0800

    Addes articles slice that pulls top headlines

--- client/src/features/store.tsx
+++ client/src/features/store.tsx
@@ -1,10 +1,15 @@
 import authReducer from "./auth";
+import articleReducer from "./articles";
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    article: articleReducer
   },
 });
-export type RootState = ReturnType<typeof store.getState>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
 export default store;
