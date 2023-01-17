357e8275825a16d08ed1c6f5a8711c920728e938
arjunsingh14 <arjunsingh140601@gmail.com>
2023-01-17 13:54:08 +0800

    Updated article routes

--- server/src/routes/articles.ts
+++ server/src/routes/articles.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getHeadlines } from "../controllers/articles";
+import { getHeadlines, getSources } from "../controllers/articles";
 const router = Router();
 
 router.route("/articles").get(getHeadlines);
+router.route("/articles/:sources").get(getSources);
 
 export default router;
