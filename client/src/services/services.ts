8b56a08b13a38a0e56a45c70dbdeafdb6d21f9e3
arjunsingh14 <arjunsingh140601@gmail.com>
2023-01-21 14:57:35 +0800

    Tokens and users saved in localstorage

---
+++ client/src/services/services.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+const baseURL = "http://localhost:3001/api/v1/";
+interface loginCredentials {
+  email: string;
+  password: string;
+}
+interface registerCredentials {
+  email: string ;
+  username: string;
+  password: string;
+}
+
+
+const loginUser = async (credentials: loginCredentials) => {
+  try {
+    const res = await axios.post(baseURL + "login", credentials);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const registerUser = async (credentials: registerCredentials) => {
+  try {
+    const res = await axios.post(baseURL + "register", credentials);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+
+export default { loginUser, registerUser };
